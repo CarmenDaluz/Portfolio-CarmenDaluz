c9a1354ac987593c36d110759791078bcad9ac2b
CarmenDaluz <daluuz.fernandez.carmen@gmail.com>
2026-04-28 19:21:25 +0200

    redesign landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -8,13 +8,13 @@ const LandingPage = () => {
         <>
             <section className="LPcontainer">
                 <div>
-                    <h1>¡¡HOLA SOY CARMEN!!</h1>
-                    <h2>FULL-STACK DEVELOPER</h2>
+                    <h1 >Hello my name is Carmen!</h1>
+                    <h2>FRONTEND DEVELOPER</h2>
                     <p>Developing, learning and designing with code. <br /> Frontend developer · Visual thinker </p>
                     <p className="nine">  "Good design is good code — and vice versa." </p>                
                 </div>
 
-                <img className='imgCode' src="/img.png" alt="retro window web paint" />
+                <img className='imgCode' src="/codeimg.png" alt="retro window web paint" />
             </section>
         </>
     )
