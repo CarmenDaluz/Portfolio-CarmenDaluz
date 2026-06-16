f93f9ddbd3e9906ba7bcb74d9feda026beaa1ebf
CarmenDaluz <daluuz.fernandez.carmen@gmail.com>
2026-06-16 14:11:44 +0200

    conditional for button href or a

--- src/components/ProjectsCard/ProjectsCard.jsx
+++ src/components/ProjectsCard/ProjectsCard.jsx
@@ -1,19 +1,19 @@
 import { useState } from 'react';
 import './ProjectsCard.css'
-import Button from '../Button/Button'
+import ButtonSmall from '../Button/ButtonSmall'
 
 const ProjectsCard = ({title, desc1, desc2, photo}) => {
     const [close, setClose] = useState(false);
 
 
     return(
-        <article className="pc-card" onClick={() => setClose(!close)}>
+        <article className="pc-card" >
             <h2 >{title}</h2>
-            <img src={photo} alt="project photo" className='projects-img'/>
+            <img src={photo} alt="project photo" className='projects-img'onClick={() => setClose(!close)}/>
             {close && <p>{desc1}</p>}
             {close && <div className='desc2'>{desc2}</div>}
-            {close && <Button to='/aboutme' title="Deploy"></Button>}
-            {close && <Button to='/aboutme' title="Github"></Button>}
+            {close && <ButtonSmall href='https://github.com/CarmenDaluz' title="Deploy"></ButtonSmall>}
+            {close && <ButtonSmall href='https://github.com/CarmenDaluz' title="Github"></ButtonSmall>}
             
         </article>
 
